f3721a544318836faeaf7d8a3730fba9acd94a1a
Oscar Ricaud <oiricaud@miners.utep.edu>
2017-03-09 00:08:54 -0700

    #8 done.

--- app/src/main/java/edu/utep/cs/cs4330/mobileappsexam1/MainActivity.java
+++ app/src/main/java/edu/utep/cs/cs4330/mobileappsexam1/MainActivity.java
@@ -1,21 +1,50 @@
 package edu.utep.cs.cs4330.mobileappsexam1;
 
-import android.support.v7.app.AppCompatActivity;
+
+
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
     private CanvasView canvasView;
     private TextView xCoordinate;
     private TextView yCoordinate;
+
+    // Save values
+    float xState = 0;
+    float yState = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // find the canvas view and register OnTouchListener
         canvasView = (CanvasView) findViewById(R.id.canvasView);
+        xCoordinate = (TextView) findViewById(R.id.textViewX);
+        yCoordinate = (TextView) findViewById(R.id.textViewY);
+
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if(extras == null) {
+                xState = 0; // Catch when the the floating numbers are empty
+                yState = 0;
+            } else{
+                xState = extras.getFloat("x");
+                yState = extras.getFloat("y");
+            }
+        } else {
+            xState = (float) savedInstanceState.getSerializable("x");
+            yState = (float) savedInstanceState.getSerializable("y");
+            xCoordinate.setText(String.valueOf(xState));
+            yCoordinate.setText(String.valueOf(yState));
+            canvasView.setxDraw(xState);
+            canvasView.setyDraw(yState);
+        }
 
         canvasView.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -25,9 +54,9 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-
         // 2. THE REST OF CODE FOR ONCREATE METHOD HERE.
     }
+
     /** Called when the screen is touched at position x and y */
     protected void screenTouched(float x, float y) {
         xCoordinate = (TextView) findViewById(R.id.textViewX);
@@ -37,5 +66,14 @@ public class MainActivity extends AppCompatActivity {
         canvasView.setxDraw(x);
         canvasView.setyDraw(y);
         canvasView.drawHere();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save the user's current game state
+        savedInstanceState.putFloat("x", canvasView.getxDraw());
+        savedInstanceState.putFloat("y", canvasView.getyDraw());
+        // Always call the superclass so it can save the view hierarchy state
+        super.onSaveInstanceState(savedInstanceState);
     }
 }
